f68f06539ed5f31bc60a7bec29e2269d294421d7
Kim Hieu Tran <kimhieu@Kim-Hieu-MBA.local>
2014-11-30 01:11:42 +0700

    Finish push

--- TechCamp/AppDelegate.m
+++ TechCamp/AppDelegate.m
@@ -28,8 +28,39 @@
     
     [[YAPushNotificationService sharedService] application:application didFinishLaunchingWithOptions:launchOptions];
     
+    // iOS 8 PN
+    
+    #ifdef __IPHONE_8_0
+        //Right, that is the point
+        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:(UIRemoteNotificationTypeBadge
+                                                                                             |UIRemoteNotificationTypeSound
+                                                                                             |UIRemoteNotificationTypeAlert) categories:nil];
+        [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+    #else
+        //register to receive notifications
+        UIRemoteNotificationType myTypes = UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound;
+        [[UIApplication sharedApplication] registerForRemoteNotificationTypes:myTypes];
+    #endif
+    
     return YES;
 }
+
+#ifdef __IPHONE_8_0
+- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings
+{
+    //register to receive notifications
+    [application registerForRemoteNotifications];
+}
+
+- (void)application:(UIApplication *)application handleActionWithIdentifier:(NSString *)identifier forRemoteNotification:(NSDictionary *)userInfo completionHandler:(void(^)())completionHandler
+{
+    //handle the actions
+    if ([identifier isEqualToString:@"declineAction"]){
+    }
+    else if ([identifier isEqualToString:@"answerAction"]){
+    }
+}
+#endif
 							
 - (void)applicationWillResignActive:(UIApplication *)application
 {
@@ -91,10 +122,13 @@
 - (void)application:(UIApplication*)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData*)deviceTokenData {
     
     [[YAPushNotificationService sharedService] application:application didRegisterForRemoteNotificationsWithDeviceToken:deviceTokenData];
+        NSLog(@"Did Register for Remote Notifications with Device Token (%@)", deviceTokenData);
 }
 
 - (void)application:(UIApplication*)application didFailToRegisterForRemoteNotificationsWithError:(NSError*)error{
 	[[YAPushNotificationService sharedService] application:application didFailToRegisterForRemoteNotificationsWithError:error];
+    NSLog(@"Did Fail to Register for Remote Notifications");
+    NSLog(@"%@, %@", error, error.localizedDescription);
 }
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
